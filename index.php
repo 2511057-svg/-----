acbeb5fa4c1c1769a0243ad4b73c48da879c34e1
藤田敦士 <2511057@i-seifu.jp>
2026-07-31 09:40:41 +0900

    revel1

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php echo "Hello, World!"; ?>
-</body>
-</html>
+<?php
+echo "<h1>レベル1 クリア！</h1>";
+echo "<p>現在の日時: " . date("Y-m-d H:i:s") . "</p>";
+?>
